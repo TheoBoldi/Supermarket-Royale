2bd16f2007ea9afcb2a566c7658f2e137de9ed1c
TheoBoldi <theo.boldireff@devinci.fr>
2019-10-25 14:08:55 +0200

    caddie prefab

--- Platinium Project/Assets/Scripts/CaddieCollision.cs
+++ Platinium Project/Assets/Scripts/CaddieCollision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CaddieCollision : MonoBehaviour
 {
+    public ParticleSystem spark;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,7 @@ public class CaddieCollision : MonoBehaviour
         if(other.gameObject.CompareTag("Caddie"))
         {
             Debug.Log("Collision Caddie-Caddie");
+            spark.Emit(1);
         }
     }
 }
