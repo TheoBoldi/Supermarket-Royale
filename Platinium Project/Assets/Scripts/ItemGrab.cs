975d3738d47885a6ef53d9de56dcfc9b7e52485b
TheoBoldi <theo.boldireff@edu.devinci.fr>
2019-11-13 15:41:19 +0100

    caddie fixed + grab /drop item

---
+++ Platinium Project/Assets/Scripts/ItemGrab.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrab : MonoBehaviour
+{
+    public GameObject item;
+    public Transform itemPos;
+    public bool haveItem = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ItemGrabing()
+    {
+        if(item != null)
+        {
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            item.transform.position = itemPos.transform.position;
+            item.transform.rotation = itemPos.transform.rotation;
+            item.transform.parent = itemPos;
+            haveItem = true;
+        }
+    }
+
+    public void ItemDroping()
+    {
+        item.transform.parent = null;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        haveItem = false;
+        item = null;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            Debug.Log("item en face");
+            if (!haveItem)
+            {
+                item = other.gameObject;
+            }
+        }
+    }
+}
