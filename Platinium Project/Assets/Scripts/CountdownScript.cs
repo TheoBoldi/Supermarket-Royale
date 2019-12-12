eb3a5e9cd54c7d6c377fb998166597a453faebc6
Victor <victor.chiorean@orange.fr>
2019-12-12 13:28:46 +0100

    Changements UI + fondu blanc

--- Platinium Project/Assets/Scripts/CountdownScript.cs
+++ Platinium Project/Assets/Scripts/CountdownScript.cs
@@ -6,8 +6,21 @@ public class CountdownScript : MonoBehaviour
 {
     public AudioSource Countdown;
 
+    public GameObject UIJ1;
+    public GameObject UIJ2;
+    public GameObject UIJ3;
+    public GameObject UIJ4;
+
     public void PlayCountdown()
     {
         Countdown.Play();
+    }
+
+    public void HideUI()
+    {
+        UIJ1.SetActive(false);
+        UIJ2.SetActive(false);
+        UIJ3.SetActive(false);
+        UIJ4.SetActive(false);
     }
 }
---
+++ Platinium Project/Assets/Scripts/NextScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    private int next;
+
+    public GameObject fondusortie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        next = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+
+    public void NextS()
+    {
+        //if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        //{
+            fondusortie.SetActive(true);
+            Invoke("nextSS", 1);
+        //}
+            
+    }
+    private void nextSS()
+    {
+        SceneManager.LoadScene(next);
+    }
+    
+}
